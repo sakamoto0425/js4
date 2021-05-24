8f43ce764c5b6621ff98cca8bb3b8801f63df24c
EC2 Default User <ec2-user@ip-172-31-72-240.ec2.internal>
2021-05-24 07:08:51 +0000

    課題完了

---
+++ main4.js
@@ -0,0 +1,11 @@
+$(function ()
+{
+    $(".slider").slick({
+        arrows: true, 
+        dots: true, 
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1500,
+    });
+});
